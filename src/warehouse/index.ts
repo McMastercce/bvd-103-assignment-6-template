20f9ba2790855d7fccdb233c0f7a510325eaf99d
Lee-Orr <leeorr@gmail.com>
2024-04-02 00:12:55 +0000

    place on shelf

--- src/warehouse/index.ts
+++ src/warehouse/index.ts
@@ -1,13 +1,9 @@
 import { type ZodRouter } from 'koa-zod-router'
-import { placeBooksOnShelfRouter } from './place_on_shelf'
 import { placeOrderRouter } from './place_order'
 import { listOrdersRouter } from './list_orders'
 import { fulfilOrderRouter } from './fulfil_order'
 
 export function setupWarehouseRoutes (router: ZodRouter): void {
-  // Placing Books on Shelves
-  placeBooksOnShelfRouter(router)
-
   placeOrderRouter(router)
 
   listOrdersRouter(router)
--- src/warehouse/warehouse.route.ts
+++ src/warehouse/warehouse.route.ts
@@ -1,10 +1,11 @@
-import { Get, Path, Route } from 'tsoa'
+import { Controller, Get, Path, Put, Route, SuccessResponse } from 'tsoa'
 import { getBookInfo } from './get_book_info'
 import { getDefaultWarehouseData } from './warehouse_data'
-import { type BookID } from './documented_types'
+import { type ShelfId, type BookID } from './documented_types'
+import { placeBooksOnShelf } from './place_on_shelf'
 
 @Route('warehouse')
-export class WarehouseRoutes {
+export class WarehouseRoutes extends Controller {
   /**
      * Find the shelves that have copies of the book, and how
      * many copies each shelf has
@@ -16,5 +17,12 @@ export class WarehouseRoutes {
     @Path() book: BookID
   ): Promise<Record<string, number>> {
     return await getBookInfo(await getDefaultWarehouseData(), book)
+  }
+
+  @Put('{book}/{shelf}/{number}')
+  @SuccessResponse(201, 'Added')
+  public async placeBooksOnShelf (@Path() book: BookID, @Path() shelf: ShelfId, @Path() number: number): Promise<void> {
+    this.setStatus(201)
+    await placeBooksOnShelf(await getDefaultWarehouseData(), book, number, shelf)
   }
 }
